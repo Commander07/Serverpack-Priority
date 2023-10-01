ce1ae4ef21789e6034bada2f85a21da90f3308ab
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-10-01 02:18:25 +0200

    feat: updated to 1.20.2

--- src/main/java/io/github/commander07/serverpackpriority/mixin/ServerResourcePackProviderMixin.java
+++ src/main/java/io/github/commander07/serverpackpriority/mixin/ServerResourcePackProviderMixin.java
@@ -11,10 +11,10 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public class ServerResourcePackProviderMixin {
 	@ModifyArgs(
 			method = "loadServerPack(Ljava/io/File;Lnet/minecraft/resource/ResourcePackSource;)Ljava/util/concurrent/CompletableFuture;",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;of(Ljava/lang/String;Lnet/minecraft/text/Text;ZLnet/minecraft/resource/ResourcePackProfile$PackFactory;Lnet/minecraft/resource/ResourcePackProfile$Metadata;Lnet/minecraft/resource/ResourceType;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)Lnet/minecraft/resource/ResourcePackProfile;")
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/resource/ResourcePackProfile;of(Ljava/lang/String;Lnet/minecraft/text/Text;ZLnet/minecraft/resource/ResourcePackProfile$PackFactory;Lnet/minecraft/resource/ResourcePackProfile$Metadata;Lnet/minecraft/resource/ResourcePackProfile$InsertionPosition;ZLnet/minecraft/resource/ResourcePackSource;)Lnet/minecraft/resource/ResourcePackProfile;")
 	)
 	private void loadServerPack(Args args) {
-		args.set(6, ResourcePackProfile.InsertionPosition.BOTTOM);
-		args.set(7, false);
+		args.set(5, ResourcePackProfile.InsertionPosition.BOTTOM);
+		args.set(6, false);
 	}
 }
