5e31a62bd3bb669137090cec6ca195697fdb4916
Commander <45269106+Commander07@users.noreply.github.com>
2024-08-07 22:55:55 +0200

    feat: update to 1.21

--- src/main/java/io/github/commander07/serverpackpriority/mixin/InsertionPositionMixin.java
+++ src/main/java/io/github/commander07/serverpackpriority/mixin/InsertionPositionMixin.java
@@ -1,8 +1,11 @@
 package io.github.commander07.serverpackpriority.mixin;
 
 import io.github.commander07.serverpackpriority.ServerpackPriority;
+import net.minecraft.resource.ResourcePackPosition;
 import net.minecraft.resource.ResourcePackProfile;
+import net.minecraft.resource.ResourcePackSource;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -13,12 +16,18 @@ import java.util.function.Function;
 
 
 @Mixin(ResourcePackProfile.InsertionPosition.class)
-public class InsertionPositionMixin {
-    @Inject(method = "insert", at= @At(value = "INVOKE", target = "Ljava/util/List;add(ILjava/lang/Object;)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    public <T> void insert(List<T> items, T item, Function<T, ResourcePackProfile> profileGetter, boolean listInverted, CallbackInfoReturnable<Integer> cir, ResourcePackProfile.InsertionPosition insertionPosition, int i) {
-        ResourcePackProfile rpp = (ResourcePackProfile) item;
-        if ((insertionPosition == ResourcePackProfile.InsertionPosition.BOTTOM) && (rpp.getName().equals("server"))) {
-            ServerpackPriority.LOGGER.info("Found server resource pack changing priority to be just above Minecraft)");
+public abstract class InsertionPositionMixin {
+    @Inject(method = "insert", at= @At(value = "HEAD"), cancellable = true)
+    public <T> void insert(List<T> items, T item, Function<T, ResourcePackPosition> profileGetter, boolean listInverted, CallbackInfoReturnable<Integer> cir) {
+        int i;
+        ResourcePackPosition resourcePackPosition;
+        if (((ResourcePackProfile) item).getSource() == ResourcePackSource.SERVER) {
+            for(i = 0; i < items.size(); ++i) {
+                resourcePackPosition = (ResourcePackPosition)profileGetter.apply(items.get(i));
+                if (!resourcePackPosition.fixedPosition() || resourcePackPosition.defaultPosition() != ResourcePackProfile.InsertionPosition.TOP) {
+                    break;
+                }
+            }
             i += 2;
             items.add(i, item);
             cir.setReturnValue(i);
